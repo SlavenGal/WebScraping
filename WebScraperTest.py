9dcaa69c1cc1704b74933eb2aacc6a7e940532e3
SlavenGal <s2galic@gmail.com>
2017-06-12 19:01:15 +0200

    Create WebScraperTest.py

--- WebScraperTest.py
+++ WebScraperTest.py
@@ -1,13 +1,16 @@
-from urllib2 import urlopen
-from bs4 import BeautifulSoup
+import os #OS module koji se poziva kako cmd ne bi zatvorio prozor nakon odrađivanja skripte
+from urllib2 import urlopen #učitavanje funkcije urlopen iz biblioteke urllib
+from bs4 import BeautifulSoup #učitavanje funkcije bs4 iz biblioteke Beautiful Soup
+import re #učitavanje modula za regularne izraze
 
-import re
+url = "https://en.wikipedia.org/wiki/Main_Page" #definirana varijabla koja prima poveznicu 
+html = urlopen(url) #korištenje funkcije urlopen i spremanje u varijablu html
+bs = BeautifulSoup(html, "lxml") #pozivanje funkcije bs koja prikazuje parsirani tekst u normalnom obliku
 
-url = "https://en.wikipedia.org/wiki/Main_Page"
-html = urlopen(url)
-bs = BeautifulSoup(html, "lxml")
-
-links = bs.find("div",{"id" : "bodyContent"}).findAll("a", href=re.compile("(/wiki/)+([A-Za-z0-9_:()])+"))
+links = bs.find("div",{"id" : "bodyContent"}).findAll("a", href=re.compile("(/wiki/)+([A-Za-z0-9_:()])+")) 
+#dva definirana "uvjeta" po kojima scraper bira podatke za povlačenje
 
 for link in links:
-	print(link['title'], link['href'])
+	print(link['title'], link['href']) #petlja za prikaz dobivenih podataka
+	
+os.system("pause") 
